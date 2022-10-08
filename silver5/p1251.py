00d0b058f44290a51e91c2a99cba8968eaaf2150
Alloy <erik1201@naver.com>
2022-10-08 16:47:32 +0900

    1

---
+++ silver5/p1251.py
@@ -0,0 +1,21 @@
+
+x= input()
+firstcut = []
+secondcut = []
+thirdcut = []
+dab = []
+answer = x + '~'
+for i in range(1, len(x)-1):
+    for j in range(i+1,len(x)):
+         firstcut = x[:i]
+         secondcut = x[i:j]
+         thirdcut = x[j::]
+         firstcut_r = firstcut[::-1]
+         secondcut_r = secondcut[::-1]
+         thirdcut_r = thirdcut[::-1]
+         dab = firstcut_r + secondcut_r + thirdcut_r
+         answer = min(answer,dab)
+
+print(answer)
+
+         
