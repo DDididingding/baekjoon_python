937d6cb44c36db7ead2688a3bba956239322cdde
Alloy <erik1201@naver.com>
2022-11-01 17:15:45 +0900

    어케하징

--- bronze1/p8595.py
+++ bronze1/p8595.py
@@ -1,1 +1,16 @@
 N = int(input())
+secretcode = input()
+sc = []
+hob = 0
+
+for i in range(N):
+    if (secretcode[i] >= '0' and secretcode[i] <= '9'):
+        if (secretcode[i+1] >= '0' and secretcode[i+1] <= '9'):
+            sc.append(secretcode[i])
+        else:
+            sc.append(secretcode[i])
+            hob = hob + int(*sc)
+            sc = []
+
+print(hob)
+        
