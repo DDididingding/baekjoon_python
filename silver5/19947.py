db2365056eecbc39084ed82464baff7eb4c221a4
Alloy <erik1201@naver.com>
2022-11-08 16:18:02 +0900

    다음에 다시 생각해보자

--- silver5/19947.py
+++ silver5/19947.py
@@ -1,26 +1,22 @@
 H, Y = map(int,input().split())
 maxmoney = 0
 
-
-if Y//5 == 0:
-    if Y//3 ==0:
+if Y<5:
+    if Y<3:
         money = H*(1.05**Y)
-        if maxmoney <= money :
+        if maxmoney <= money:
             maxmoney = money
-    else:
-        money = H*(1.2**(Y//3)+1.05**(Y%3))
-        if maxmoney <= money :
+    else :
+        money = H*(1.2**(Y//3))*(1.05**(Y%3))
+        if maxmoney <= money:
             maxmoney = money
 else:
-    money = H*(1.2**(Y//3)+1.05**(Y%3))
-    if maxmoney <= money :
-         maxmoney = money
-
-    money = H*(1.35**(Y//5)+1.05**(Y%5))
-    if maxmoney <= money :
+    money = H*(1.2**(Y//3))*(1.05**(Y%3))
+    if maxmoney <= money:
         maxmoney = money
-    if (Y//5)//3 != 0:
-        money = H*(1.35**(Y//5)+1.2**((Y//5)//3)+1.05**((Y%5)%3))
+    
+    money = H*(1.)
+
 
 print(maxmoney)
 
