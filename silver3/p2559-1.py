e73524283852a58781930486ef2b5e00ad2c6138
Alloy <erik1201@naver.com>
2022-11-22 19:52:32 +0900

    sujung

--- silver3/p2559-1.py
+++ silver3/p2559-1.py
@@ -1,23 +1,17 @@
 N,K = map(int, input().split())
 
 numbers = []
-numbers = input().split()
+numbers = list(map(int,input().split()))
 summation = 0
 
 
 for i in range(K):
-    temp = numbers[i]
-    temp = int(temp)
-    summation = summation + temp
+    summation = summation + numbers[i]
     best = summation
 
 
 for i in range(N - K):
-    temp = numbers[i]
-    temp = int(temp)
-    temp1 = numbers[i+K]
-    temp1 = int(temp1)
-    summation = summation - temp + temp1
+    summation = summation - numbers[i] + numbers[i+K]
 
     if best < summation:
         best = summation
